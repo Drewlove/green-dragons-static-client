9ae739ad43d7fb1b5e76388214a33737764f9e85
Drew Love <davidandrewlove84@gmail.com>
2020-04-01 08:47:01 -0400

    testing development

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Route, BrowserRouter} from 'react-router-dom'
+import {Route} from 'react-router-dom'
 import Nav from './ReusableComponents/Nav'
 import ChallengesRouting from './Routing/ChallengesRouting'
 
