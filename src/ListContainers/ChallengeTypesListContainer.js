441719252e2feda6664d059f512a910830c15d46
Drew Love <davidandrewlove84@gmail.com>
2020-04-01 09:17:33 -0400

    reduce to 2 challenge types

--- src/ListContainers/ChallengeTypesListContainer.js
+++ src/ListContainers/ChallengeTypesListContainer.js
@@ -6,10 +6,6 @@ const ChallengeTypesListContainer = () => {
     const challengesList = [
         {name: 'Challenge 1', path: '/challenges/challenge-1', id: 1}, 
         {name: 'Challenge 2', path: '/challenges/challenge-2', id: 2}, 
-        {name: 'Challenge 3', path: '/challenges/challenge-3', id: 3},
-        {name: 'Challenge 4', path: '/challenges/challenge-4', id: 4},
-        {name: 'Challenge 5', path: '/challenges/challenge-5', id: 5},
-        {name: 'Challenge 6', path: '/challenges/challenge-6', id: 6}
     ]
 
     const renderList = () => {
